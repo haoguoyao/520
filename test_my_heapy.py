af51fe842261f64d3f48729c65366992d89f6d4f
haoguoyao <1479815750@qq.com>
2020-02-08 21:50:28 -0500

    On branch master  Changes to be committed: 	new file:   Assignment1.pdf 	new file:   test_my_heapy.py

---
+++ test_my_heapy.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  8 21:24:57 2020
+
+@author: apple
+"""
+
+import my_heapq as heapq
+
+a = heapq.my_heapq()
+a.push(1)
+a.show()
+a.push(4)
+a.show()
+a.push(3)
+a.show()
+a.push(2)
+a.show()
+a.push(1)
+a.show()
+a.push(4)
+a.show()
+a.push(3)
+a.show()
+a.push(2)
+a.show()
+a.pop()
+a.show()
+a.pop()
+a.show()
+a.pop()
+a.show()
+a.pop()
+a.show()
+a.pop()
+a.show()
+a.pop()
+a.show()
+a.pop()
+a.show()
